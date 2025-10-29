6ecef70d16b904d14cf09c01af39f763f211eac6
Arun7427 <arunchennoju77@gmail.com>
2025-10-29 11:23:49 +0530

    Create EnumExFruitNames.java

---
+++ LABEX/EnumExFruitNames.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+enum Fruits{
+	Apple('a'), Banana('b'), Citrus('a'), DragonFruit('d'), Fig('e'),Grapes('f');
+	char a;
+	Fruits(char a){
+		this.a = a;
+	}
+	public char get(){
+		return a;
+		
+	}
+}	
+public class EnumExFruitNames{
+	public static void main(String...args){
+	Scanner s = new Scanner(System.in);
+	String st = s.next();
+	char ch = st.charAt(0);
+ 	ch = Character.toLowerCase(ch); 
+		for(Fruits f: Fruits.values()){
+			if(f.get() == ch)
+			System.out.println(f);
+		}
+	}
+}
